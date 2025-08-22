0f86d0b5a2b32a7c3e5fd217b28cec4e26a45e76
meggsan <meggie.sanchez@gmail.com>
2025-08-22 19:43:49 +0200

    :white_check_mark:  add useOrders test but now as a spy

---
+++ src/hooks/useOrdersSpy.test.tsx
@@ -0,0 +1,55 @@
+import * as ReactRouter from "react-router-dom";
+import {
+  describe,
+  it,
+  expect,
+  vi,
+  MockInstance,
+  beforeEach,
+  Mock,
+  afterEach,
+} from "vitest";
+import { renderHook, waitFor } from "@testing-library/react";
+import * as AuthContext from "../context/AuthContext";
+import { useOrders } from "./useOrders";
+import * as OrderService from "../services/getOrders";
+
+vi.mock("react-router-dom", () => ({
+  useNavigate: vi.fn(),
+}));
+
+describe("useOrdersSpy", () => {
+  let useSessionSpy: MockInstance;
+  let getOrdersSpy: MockInstance;
+  const mockNavigate = vi.fn();
+
+  beforeEach(() => {
+    useSessionSpy = vi.spyOn(AuthContext, "useSession");
+    getOrdersSpy = vi.spyOn(OrderService, "getOrders");
+
+    (ReactRouter.useNavigate as Mock).mockReturnValue(mockNavigate);
+
+    vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("should show an error with orders", async () => {
+    useSessionSpy.mockReturnValue({ user: { id: 1 } });
+    getOrdersSpy.mockRejectedValue("Api error");
+    const { result } = renderHook(() => useOrders());
+
+    expect(result.current.loading).toBe(true);
+    expect(result.current.error).toBeNull();
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.error).toBe(
+        "Failed to fetch orders. Please try again later"
+      );
+      expect(getOrdersSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+});
